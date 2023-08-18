f63c6c1c43a8fa095f57d28802b94ae398ace688
STas <108541890+stas159153@users.noreply.github.com>
2023-08-18 13:53:50 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let array = ['s','c','h','o','o','l']
+let sring =  array.join()
+let cards = [1,2,3,4,5]
+cards.splice(2,1)
+cards.splice(4, 0,6);
+console.log(sring,cards);
+// for( let w of array){
+//   sring.join(w)
+// }
